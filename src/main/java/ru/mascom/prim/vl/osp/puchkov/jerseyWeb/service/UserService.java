0a397094d3c37f317e224dbae638fb8714435eb6
Puchkov Nikita <puchkov.ni.90@gmail.com>
2018-07-19 22:05:29 +1000

    CRUD realised, but application still needs DataBase to save results

--- src/main/java/ru/mascom/prim/vl/osp/puchkov/jerseyWeb/service/UserService.java
+++ src/main/java/ru/mascom/prim/vl/osp/puchkov/jerseyWeb/service/UserService.java
@@ -5,7 +5,9 @@ import ru.mascom.prim.vl.osp.puchkov.jerseyWeb.DAO.UserDAONoDBImpl;
 import ru.mascom.prim.vl.osp.puchkov.jerseyWeb.model.User;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/users")
@@ -18,17 +20,49 @@ public class UserService {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-//    @Consumes(MediaType.APPLICATION_JSON)
-    public List<User> getUsers() {
-        return userDAO.getUsers();
+    public Response getUsers() {
+        List<User> users = userDAO.getUsers();
+        if (users == null || users.isEmpty())
+            return Response.status(Response.Status.NOT_FOUND).build();
+
+        GenericEntity<List<User>> result = new GenericEntity<List<User>>(users){};
+
+        return Response.ok(result).build();
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-//    @Consumes(MediaType.APPLICATION_JSON)
-    public User getUsers(@PathParam("id") Integer id) {
-        return userDAO.getUser(id);
+    public Response getUser(@PathParam("id") Integer id) {
+        User result = userDAO.getUser(id);
+        if (result == null)
+            return Response.status(Response.Status.NOT_FOUND).build();
+        return Response.ok(userDAO.getUser(id)).build();
     }
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addUser(User user) {
+        if (!userDAO.addUser(user))
+            return Response.status(Response.Status.NOT_MODIFIED).build();
+        return Response.ok(user).build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateUser(User user) {
+        if (!userDAO.updateUser(user))
+            return Response.status(Response.Status.NOT_MODIFIED).build();
+        return Response.ok(user).build();
+    }
+
+    @DELETE
+    @Path("{id}") //according to {@link https://habr.com/post/38730/}
+    public Response deleteUser(@PathParam("id") Integer id) {
+        if (!userDAO.deleteUser(id))
+            return Response.status(Response.Status.NOT_MODIFIED).build();
+        return Response.ok().build();
+    }
 }
